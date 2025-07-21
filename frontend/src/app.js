da5fd8a054c3c284608f65b1bf300ddd8de62fe6
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-07-21 15:06:54 +0530

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,13 @@
- 
+import React from 'react';
+import ServerSwitch from './components/ServerSwitch';
+
+function App() {
+  return (
+    <div>
+      <h1>ZBØTS Configuration Panel</h1>
+      <ServerSwitch />
+    </div>
+  );
+}
+
+export default App; 
